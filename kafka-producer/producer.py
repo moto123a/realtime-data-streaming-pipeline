35434026732ab9a9865116fedc1f591340e60dd4
moto123a <74400139+moto123a@users.noreply.github.com>
2026-02-19 12:42:48 -0600

    Refactor producer to send shipment events instead of orders

--- kafka-producer/producer.py
+++ kafka-producer/producer.py
@@ -9,22 +9,24 @@ producer = KafkaProducer(
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
-products = ["P-100", "P-200", "P-300", "P-400"]
-customers = ["C-1", "C-2", "C-3", "C-4", "C-5"]
+origins = ["Chicago Rail Yard", "Dallas Hub", "Houston Freight Port", "Denver Depot"]
+destinations = ["Atlanta Terminal", "Seattle Hub", "Phoenix Yard", "Miami Freight Station"]
+transport_modes = ["Rail", "Truck"]
+shipment_status = ["In Transit", "Delayed", "Delivered", "At Warehouse"]
 
 while True:
-    event = {
-        "event_time": datetime.utcnow().isoformat(),
-        "order_id": f"O-{random.randint(100000,999999)}",
-        "customer_id": random.choice(customers),
-        "product_id": random.choice(products),
-        "qty": random.randint(1,5),
-        "unit_price": round(random.uniform(50,250),2)
+    shipment_event = {
+        "shipment_id": f"SHP-{random.randint(10000,99999)}",
+        "origin": random.choice(origins),
+        "destination": random.choice(destinations),
+        "transport_mode": random.choice(transport_modes),
+        "shipment_status": random.choice(shipment_status),
+        "weight_tons": random.randint(10,100),
+        "distance_km": random.randint(100,2000),
+        "event_timestamp": datetime.utcnow().isoformat()
     }
-    event["total_amount"] = event["qty"] * event["unit_price"]
 
-    producer.send("orders-topic", event)
-    print("[Producer] Sent:", event)
+    producer.send("shipment_events", shipment_event)
+    print("[Shipment Producer] Sent:", shipment_event)
 
     time.sleep(1)
-
