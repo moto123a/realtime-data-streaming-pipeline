6363f1d0dd4f9ee0fb7df2f0b168344d6382763e
moto123a <74400139+moto123a@users.noreply.github.com>
2026-02-19 12:30:36 -0600

    Create orders_streaming table in schema.sql

--- data-warehouse/schema.sql
+++ data-warehouse/schema.sql
@@ -1,1 +1,10 @@
+CREATE TABLE orders_streaming (
+    event_time VARCHAR(50),
+    order_id VARCHAR(20),
+    customer_id VARCHAR(10),
+    product_id VARCHAR(10),
+    qty INT,
+    unit_price FLOAT,
+    total_amount FLOAT
+);
 
